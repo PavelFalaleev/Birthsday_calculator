8ecaf686b30bd0593474a97fa340c377c957126b
Pavel Falaleev <pasf@yandex.ru>
2023-10-05 00:37:51 +0300

    start decor

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 from django.conf import settings
 # Импортируем функцию, позволяющую серверу разработки отдавать файлы.
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
 
 urlpatterns = [
     path('', include('pages.urls')),
@@ -10,4 +12,13 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
     path('birthday/', include('birthday.urls')),
     # В конце добавляем к списку вызов функции static.
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
