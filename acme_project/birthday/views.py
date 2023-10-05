9caae18721aa0aec6e5c3b1a485e74d6bc3b353c
Pavel Falaleev <pasf@yandex.ru>
2023-10-05 16:54:22 +0300

    авторизация юзеров

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -2,13 +2,20 @@ from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
 )
 from django.urls import reverse_lazy
-
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from .forms import BirthdayForm
 from .models import Birthday
 from .utils import calculate_birthday_countdown
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class BirthdayListView(ListView):
+@login_required
+def simple_view(request):
+    return HttpResponse('Страница для залогиненных пользователей!')
+
+
+class BirthdayListView(LoginRequiredMixin, ListView):
     model = Birthday
     ordering = 'id'
     paginate_by = 10
@@ -19,22 +26,28 @@ class BirthdayMixin:
     success_url = reverse_lazy('birthday:list')
 
 
-class BirthdayCreateView(BirthdayMixin, CreateView):
+class BirthdayCreateView(LoginRequiredMixin, BirthdayMixin, CreateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+    def form_valid(self, form):
+        # Присвоить полю author объект пользователя из запроса.
+        form.instance.author = self.request.user
+        # Продолжить валидацию, описанную в форме.
+        return super().form_valid(form) 
+
+
+class BirthdayUpdateView(LoginRequiredMixin, BirthdayMixin, UpdateView):
     model = Birthday
     form_class = BirthdayForm
 
 
-class BirthdayUpdateView(BirthdayMixin, UpdateView):
-    model = Birthday
-    form_class = BirthdayForm
-
-
-class BirthdayDeleteView(BirthdayMixin, DeleteView):
+class BirthdayDeleteView(LoginRequiredMixin, BirthdayMixin, DeleteView):
     model = Birthday
     pass
 
 
-class BirthdayDetailView(DetailView):
+class BirthdayDetailView(LoginRequiredMixin, DetailView):
     model = Birthday
 
     def get_context_data(self, **kwargs):
